5702285d453000be4d90ddfb8a26943688ae47f0
JuanCarlosSanchezVazquez0401 <carloseseuvebackup1@gmail.com>
2024-04-26 01:28:48 -0600

    Reapply "Segunda version de bolita"
    This reverts commit 035bda1a47064db2fee2b0a364c4f863cde9275e.

--- Bolita/src/bolita/Bolita.java
+++ Bolita/src/bolita/Bolita.java
@@ -1,8 +1,14 @@
 
 package bolita;
 
+import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 
 public class Bolita extends Canvas {
@@ -16,10 +22,77 @@ public class Bolita extends Canvas {
     public void paint(Graphics g ){
         super.print(g);
         g.fillOval(moverX, moverY, 30, 30);
+        
     }
 
+    public int getMoverX() {
+        return moverX;
+    }
+
+    public int getMoverY() {
+        return moverY;
+    }
+
+    public void setMoverX(int moverX) {
+        this.moverX = moverX;
+    }
+
+    public void setMoverY(int moverY) {
+        this.moverY = moverY;
+    }
+    
     public static void main(String[] args) {
         
     }
     
+    // clase controlador 
+    public class Controlador extends JFrame{
+    
+        JPanel pBolita, pControl;
+        
+    public Controlador(){
+    
+        setTitle("Mover"); setSize(400,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(2,1)); setResizable(false);
+        
+    Bolita b = new Bolita();
+    pBolita = new JPanel();
+    pControl = new Control();
+    
+        
+    }
+        
+    public class Control extends JPanel implements ActionListener{
+    
+        JButton btnIzquierda, btnDerecha, btnArriba, btnAbajo;
+        Bolita bolita;
+        
+        public Control(){}
+        
+        public Control(Bolita b){
+            setLayout(new BorderLayout());
+            btnIzquierda = new JButton("Izquierda");
+            btnDerecha = new JButton("Derecha");
+            btnArriba = new JButton("Arriba");
+            btnAbajo = new JButton("Abajo");
+            
+            btnIzquierda.addActionListener(this);
+            btnDerecha.addActionListener(this);
+            btnArriba.addActionListener(this);
+            btnAbajo.addActionListener(this);
+            
+            bolita = new Bolita();
+            bolita = b;
+            
+            
+            
+            
+        }
+        
+    }
+    
+
+    
+    }
 }
